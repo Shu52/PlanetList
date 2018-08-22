fa9d928da8e78645f3046fa2182e1d9925bc3e84
Shuaib Sajid <shusajid@gmail.com>
2018-08-22 11:10:25 -0500

    planetList exercise completed, will branch for iterating over planets

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lists
+{
+    using System.Collections.Generic; // Namespace containing data structures
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>(){"Mercury", "Mars"};
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+            List<string> morePlanetList = new List<string>(){"Uranus", "Neptune"};
+            planetList.AddRange(morePlanetList);
+            planetList.Insert( planetList.IndexOf( "Jupiter" ), "Earth" );
+            planetList.Insert( planetList.IndexOf( "Jupiter" ), "Venus" );
+            planetList.Add("Pluto");
+            List<string> rockyPlanets = planetList.GetRange(0,4);
+            planetList.Remove("Pluto");
+        }
+    }
+}
